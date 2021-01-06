365d36755b5f0f384d07d8b3c837dcbd5b8e8edf
Mahmed amer <67114727+Mahmedamer@users.noreply.github.com>
2021-01-06 09:32:30 +0200

    Add project files.

---
+++ CircuitsProject/Source.cpp
@@ -0,0 +1,34 @@
+#include<Eigen/Dense>
+#include<Eigen/sparse>
+#include<complex>
+#include<iostream>
+using namespace std;
+using namespace Eigen;
+using namespace std::complex_literals;
+int main()
+{
+
+	MatrixXcf m3(2, 2);
+	for (int i = 0; i < 2; i++)
+		for (int j = 0; j < 2; j++)
+			m3(i, j) = 2.0 + 3i;
+
+	int n = 2;
+
+	MatrixXcf m1(n, 2);
+	MatrixXcf m2(2, 1);
+	complex<double> c = 1. / (0.0005 * 1i * 1000.);
+
+	m1 << 8. - 2i, -8.,
+		-8., 14. + 1i;
+
+	m1(0, 0) = 8. - 2i;
+
+	m2 << 10. + 6i, -24. - 35i;
+
+	// m*X=B--->X=m.inverse()*B
+
+	cout << m1.inverse() * m2;
+
+	return 0;
+}
